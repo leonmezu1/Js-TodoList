18480206aac37d7f229f020d3fb56da6351c7a6a
leonmezu1 <leo.mez@outlook.com>
2020-06-12 16:55:46 -0500

    App mockup completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import '../styles/styles.scss';
 
 console.log('Test 2');
+
+const navigation = document.getElementById('navigation');
+const mainView = document.getElementById('main-view');
+
+const height = navigation.offsetHeight;
+
+mainView.style.height = `${window.innerHeight - height}px`;
+console.log(window.innerHeight - height);
