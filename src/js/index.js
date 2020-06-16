8142766a9dee228a9eef38cf438cd00306dba20d
Raymond Oluoch <rasholuoch@gmail.com>
2020-06-16 22:43:19 +0300

    adding event listener for adding project names

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,29 @@
 import '../styles/styles.scss';
 
-console.log('Test 2');
-
 const navigation = document.getElementById('navigation');
 const mainView = document.getElementById('main-view');
 
 const height = navigation.offsetHeight;
 
 mainView.style.height = `${window.innerHeight - height}px`;
-console.log(window.innerHeight - height);
+
+const projectname = document.getElementById('projectName');
+
+const ttitle = document.getElementById('tTitle').value;
+const tdescription = document.getElementById('tDescription').value;
+const tduedate = document.getElementById('tDueDate').value;
+const tpriority = document.getElementById('tPriority').value;
+const tproject = document.getElementById('tProject').value;
+
+
+const addProjectName = (pname) => {
+  e.preventDefault();
+};
+
+projectname.addEventListener('submit', () => {
+  const pname = document.getElementById('pName').value;
+  const project = document.createElement('p');
+  project.innerText = pname;
+  const container = document.getElementById('projects-container');
+  container.appendChild(project);
+});
